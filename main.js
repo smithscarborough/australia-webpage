fbf76a2e44eb6633637e87382ace59a9ff550b08
Smith Scarborough <smithscarborough@gmail.com>
2021-02-12 11:04:28 -0600

    adds style & js

---
+++ main.js
@@ -0,0 +1,6 @@
+const punchlineBtn = document.getElementById('button');
+const joke = document.getElementsByName('h1');
+
+punchlineBtn.addEventListener('click', () => {
+    alert('at IHOP! 😂')
+})
